9e00fa048ece83c8015564ae7fda8131cbcf1611
devopsavinashpandey-indore <devopsavinashpandey@gmail.com>
2026-04-11 11:59:17 +0530

    Add modules for VPC, EC2, and S3 in main.tf

---
+++ main.tf
@@ -0,0 +1,19 @@
+module "vpc" {
+  source = "./modules/vpc"
+
+  cidr_block = var.vpc_cidr
+}
+
+module "ec2" {
+  source = "./modules/ec2"
+
+  ami_id         = var.ami_id
+  instance_type  = var.instance_type
+  subnet_id      = module.vpc.subnet_id
+}
+
+module "s3" {
+  source = "./modules/s3"
+
+  bucket_name = var.bucket_name
+}
