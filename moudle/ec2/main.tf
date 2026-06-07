bcdecae654c41cd925cf8d143149e4515e200dba
devopsavinashpandey-indore <devopsavinashpandey@gmail.com>
2026-06-07 11:55:09 +0530

    Update main.tf

--- moudle/ec2/main.tf
+++ moudle/ec2/main.tf
@@ -16,3 +16,10 @@ resource "aws_instance" "ec2" {
     Name = "Terraform-EC2"
   }
 }
+resource "aws_instance" "ec2" {
+  ami           = var.ami_id
+  instance_type = var.instance_type
+  subnet_id     = var.subnet_id
+
+  tags = {
+    Name = "Terraform-EC2"
